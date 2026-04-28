0976a1ef4b9b5b10a19205521633a2f703c147a0
Andrea Massironi <massironi.andrea@gmail.com>
2026-04-28 17:44:04 +0200

    snapshot

---
+++ WW/2018/snapshot.py
@@ -0,0 +1,28 @@
+
+#
+# this must match the one defined in cuts.py
+#
+cut_to_dump = "DY_cr_eleele"
+
+#
+# where to save the ttrees
+#
+folder_where_to_save_trees = "/eos/user/a/amassiro/HWW/2026/snapshot/"
+
+#
+# the variables to save
+# I can also save directly the variables defined in aliases.py and variables.py
+#
+variables_to_dump.append("pt1")
+variables_to_dump.append("mll")
+variables_to_dump.append("ptj1")
+variables_to_dump.append("njet")
+variables_to_dump.append("mumu")
+
+variables_to_dump.append("PromptGenLepMatch2l")
+
+variables_to_dump.append("myVariableMll")
+
+
+
+
