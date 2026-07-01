1cd449f5077fa28828b7be09acae0605f85274d0
Andrea Massironi <massironi.andrea@gmail.com>
2026-07-01 17:23:44 +0200

    2024

---
+++ WW/2024/aliases.py
@@ -0,0 +1,176 @@
+#
+# List of aliases to be "defined"
+# This list will NOT be the actual "Alias" of RDataFrame (as useless)
+# but "Define" of all this math will be performed
+#
+#
+
+# eleWP = 'cutBased_LooseID_tthMVA_Run3'
+eleWP = 'cutBased_MediumID_tthMVA_Run3'
+muWP  = 'cut_TightID_pfIsoTight_HWW_tthmva_67'
+
+
+
+aliases['PromptGenLepMatch2l'] = {
+    'expr': '(Lepton_promptgenmatched.size() > 0 ? Lepton_promptgenmatched[0] : 0.) * (Lepton_promptgenmatched.size() > 1 ? Lepton_promptgenmatched[0] : 0.)',
+    'samples': mcALL
+}
+
+
+
+
+aliases['mumu'] = {
+    'expr': '(Lepton_pdgId.size() > 0 ? Lepton_pdgId[0] : -99) * (Lepton_pdgId.size() > 1 ? Lepton_pdgId[1]: -99) == -13*13',
+    'samples': ALL
+}
+
+aliases['ee'] = {
+    'expr': '(Lepton_pdgId.size() > 0 ? Lepton_pdgId[0] : -99) * (Lepton_pdgId.size() > 1 ? Lepton_pdgId[1]: -99) == -11*11',
+    'samples': ALL
+}
+
+
+
+
+# -------- lepton WP
+aliases['LepWPCut'] = {
+    'expr': 'LepCut2l__ele_'+eleWP+'__mu_'+muWP ,
+    'samples': ALL
+}
+
+
+
+# ---------------------------- btagging
+#
+# B-Tagging WP: https://btv-wiki.docs.cern.ch/ScaleFactors/Run3Summer23/
+#
+#
+#    "UParTAK4B" : {"loose" : "0.0246", "medium" : "0.1272", "tight" : "0.4648", "xtight" : "0.6298", "xxtight" : "0.9739"},
+#
+
+aliases['bVeto'] = {
+    'expr' : 'ROOT::VecOps::Sum(Take(Jet_btagUParTAK4B,CleanJet_jetIdx)[CleanJet_pt > 20 && abs(CleanJet_eta) < 2.5] > 0.0246) == 0',
+    'samples': ALL
+}
+
+aliases['bReq'] = {
+    'expr' : 'ROOT::VecOps::Sum(Take(Jet_btagUParTAK4B,CleanJet_jetIdx)[CleanJet_pt > 20 && abs(CleanJet_eta) < 2.5] > 0.0246) >= 1',
+    'samples': ALL
+}
+
+# aliases['bVetoSF'] = {
+#     'expr' : 'ROOT::VecOps::Product(Take(Jet_btagSF_deepcsv_shape,CleanJet_jetIdx)[CleanJet_pt > 20 && abs(CleanJet_eta) < 2.5])',
+#     'samples': mcALL
+# }
+#
+# aliases['bReqSF'] = {
+#     'expr' : 'ROOT::VecOps::Product(Take(Jet_btagSF_deepcsv_shape,CleanJet_jetIdx)[CleanJet_pt > 30 && abs(CleanJet_eta) < 2.5])',
+#     'samples': mcALL
+# }
+#
+# aliases['btagSF'] = {
+#     'expr': 'bVeto*bVetoSF + bReq*bReqSF',
+#     'samples': mcALL
+# }
+
+
+
+
+
+# --------------------------- PU weights
+# aliases['Jet_PUIDSF'] = {
+#   'expr' : 'ROOT::VecOps::Product(Jet_PUIDSF_loose[Jet_jetId>=2])',
+#   'samples': mcALL
+# }
+#
+# aliases['Jet_PUIDSF_up'] = {
+#   'expr' : 'ROOT::VecOps::Product(Jet_PUIDSF_loose_up[Jet_jetId>=2])',
+#   'samples': mcALL
+# }
+#
+# aliases['Jet_PUIDSF_down'] = {
+#   'expr' : 'ROOT::VecOps::Product(Jet_PUIDSF_loose_down[Jet_jetId>=2])',
+#   'samples': mcALL
+# }
+
+
+
+aliases['LepWPSF'] = {
+    'expr': 'LepSF2l__ele_'+eleWP+'__mu_'+muWP,
+    'samples': mcALL
+}
+
+
+# see:
+# https://github.com/latinos/LatinoAnalysis/blob/master/NanoGardener/python/data/formulasToAdd_MC_2017.py
+#
+
+
+# variations
+aliases['SFweightEleUp'] = {
+    'expr': 'LepSF2l__ele_'+eleWP+'__Up',
+    'samples': mcALL
+}
+aliases['SFweightEleDown'] = {
+    # 'expr': 'LepSF2l__ele_'+eleWP+'__Do',
+    'expr': 'LepSF2l__ele_'+eleWP+'__Down',
+    'samples': mcALL
+}
+aliases['SFweightMuUp'] = {
+    'expr': 'LepSF2l__mu_'+muWP+'__Up',
+    'samples': mcALL
+}
+aliases['SFweightMuDown'] = {
+    # 'expr': 'LepSF2l__mu_'+muWP+'__Do',
+    'expr': 'LepSF2l__mu_'+muWP+'__Down',
+    'samples': mcALL
+}
+
+
+
+
+aliases['Top_pTrw'] = {
+  'expr': '(topGenPt * antitopGenPt > 0.) * (TMath::Sqrt((0.103*TMath::Exp(-0.0118*topGenPt) - 0.000134*topGenPt + 0.973) * (0.103*TMath::Exp(-0.0118*antitopGenPt) - 0.000134*antitopGenPt + 0.973))) + (topGenPt * antitopGenPt <= 0.)',
+  # 'expr': '(topGenPt * antitopGenPt > 0.) * (TMath::Sqrt(TMath::Exp(0.0615 - 0.0005 * topGenPt) * TMath::Exp(0.0615 - 0.0005 * antitopGenPt))) + (topGenPt * antitopGenPt <= 0.)',
+  'samples': ['top']
+}
+
+
+
+
+
+
+#
+# external defined variable: c++ code
+#
+
+# aliases['myVariableMll'] = {
+#   'external' : "code/mymll.c",
+#   'variables': ["Lepton_pt[0]", "Lepton_eta[0]", "Lepton_phi[0]", "Lepton_pt[1]", "Lepton_eta[1]", "Lepton_phi[1]"],
+#   'function' : 'calculate_mass',
+#   'samples': mcALL
+# }
+
+
+
+#
+# if some variables are not defined but they are needed as inputs of the TMVA, define them!
+#
+
+aliases['ptj1'] = {
+    'expr': 'CleanJet_pt.size() > 0 ? CleanJet_pt[0] : -9999.',
+    'samples' : ALL
+}
+
+aliases['njet'] = {
+    'expr': 'Sum(CleanJet_pt>30)',
+    'samples' : ALL
+}
+
+# aliases['myVariableBDT'] = {
+#   'variables': ["pt1", "ptj1", "mll", "njet"],
+#   'function' : 'TMVA',
+#   'xmlfile'  : 'code/TMVAClassification_BDTG.weights.xml',
+#   'samples': ALL
+# }
+
---
+++ WW/2024/samples.py
@@ -0,0 +1,200 @@
+#
+# Samples file
+#
+
+# import os
+
+#
+# How to get the list of files to be analysed
+#
+
+
+mcProduction   = 'Summer24_150x_nAODv15_Full2024v15'
+mcSteps        = 'MCl2loose2024v15__MCCorr2024v15__JERFrom23BPix__l2tight'
+dataRecoMuon   = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_Muon'
+dataRecoEGamma = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_EGamma'
+dataRecoMuonEG = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_MuonEG'
+dataSteps      = 'DATAl2loose2024v15__l2loose'
+
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+
+
+def makeMCDirectory(var=""):
+    _treeBaseDir = treeBaseDir + ""
+    if var == "":
+        return "/".join([_treeBaseDir, mcProduction, mcSteps])
+    else:
+        return "/".join([_treeBaseDir, mcProduction, mcSteps + "__" + var])
+
+mcDirectory = makeMCDirectory()
+# fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
+# dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
+
+fakeDirectoryMuon = os.path.join(treeBaseDir, dataRecoMuon, dataSteps)
+dataDirectoryMuon = os.path.join(treeBaseDir, dataRecoMuon, dataSteps)
+fakeDirectoryEGamma = os.path.join(treeBaseDir, dataRecoEGamma, dataSteps)
+dataDirectoryEGamma = os.path.join(treeBaseDir, dataRecoEGamma, dataSteps)
+fakeDirectoryMuonEG = os.path.join(treeBaseDir, dataRecoMuonEG, dataSteps)
+dataDirectoryMuonEG = os.path.join(treeBaseDir, dataRecoMuonEG, dataSteps)
+
+
+from search_files import SearchFiles
+searchFiles = SearchFiles()
+
+useXROOTD = False
+redirector = 'root://eoscms.cern.ch/'
+
+def nanoGetSampleFiles(path, name):
+  _files = searchFiles.searchFiles(path, name, redirector=redirector)
+  return  {name : _files}
+
+
+def addSubSampleWeights(samples, sampleName, subSampleName, weight):
+  if 'weights' not in samples[sampleName].keys():
+    samples[sampleName]['weights'] = {}
+  if subSampleName in samples[sampleName]['weights'].keys():
+    samples[sampleName]['weights'][subSampleName] = "(" + samples[sampleName]['weights'][subSampleName] + ") * " +  weight
+  else :
+    samples[sampleName]['weights'][subSampleName] = weight
+
+
+
+
+#
+# The weights
+#
+
+mcCommonWeight = 'XSWeight * SFweight2l * LepWPCut * LepWPSF * PromptGenLepMatch2l'
+# mcCommonWeight = 'XSWeight * SFweight2l * LepWPCut * LepWPSF * METFilter_MC * PromptGenLepMatch2l'
+# mcCommonWeight = 'XSWeight * SFweight2l * LepWPCut * LepWPSF * Jet_PUIDSF * METFilter_MC * PromptGenLepMatch2l'
+# mcCommonWeight = 'XSWeight * SFweight2l * LepWPCut * LepWPSF * Jet_PUIDSF * btagSF * METFilter_MC * PromptGenLepMatch2l'
+
+
+
+
+
+
+#
+# The actual list of samples
+#
+
+############ Top ############
+
+
+files = nanoGetSampleFiles(mcDirectory, 'TTTo2L2Nu') | \
+        nanoGetSampleFiles(mcDirectory, 'TbarWplusto2L2Nu') | \
+        nanoGetSampleFiles(mcDirectory, 'TWminusto2L2Nu') | \
+        nanoGetSampleFiles(mcDirectory, 'ST_t-channel_top') | \
+        nanoGetSampleFiles(mcDirectory, 'ST_t-channel_antitop') | \
+        nanoGetSampleFiles(mcDirectory, 'ST_s-channel_plus') | \
+        nanoGetSampleFiles(mcDirectory, 'ST_s-channel_minus')
+
+samples['top'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    # 'weight': mcCommonWeight + " * Top_pTrw ",
+    'FilesPerJob': 30,
+}
+
+addSubSampleWeights (samples, 'top', 'TTTo2L2Nu', 'Top_pTrw')
+
+
+############ DY ############
+
+
+files = nanoGetSampleFiles(mcDirectory, 'DYto2E-2Jets_MLL-50') | \
+        nanoGetSampleFiles(mcDirectory, 'DYto2Mu-2Jets_MLL-50') | \
+        nanoGetSampleFiles(mcDirectory, 'DYto2Tau-2Jets_MLL-50') | \
+        nanoGetSampleFiles(mcDirectory, 'DYto2E-2Jets_MLL-10to50') | \
+        nanoGetSampleFiles(mcDirectory, 'DYto2Mu-2Jets_MLL-10to50') | \
+        nanoGetSampleFiles(mcDirectory, 'DYto2Tau-2Jets_MLL-10to50')
+
+#
+# files = nanoGetSampleFiles(mcDirectory, 'DYto2L-2Jets_MLL-10to50') | \
+#         nanoGetSampleFiles(mcDirectory, 'DYto2L-2Jets_MLL-50')
+#
+
+#print (" list of files DY = ", files)
+
+samples['DY'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 50,
+}
+
+
+
+
+
+#
+# Data
+#
+
+
+DataRun = [
+    ['C','Run2024C-ReReco-v1'],
+    ['D','Run2024D-ReReco-v1'],
+    ['E','Run2024E-ReReco-v1'],
+    ['F','Run2024F-Prompt-v1'],
+    ['G','Run2024G-Prompt-v1'],
+    ['H','Run2024H-Prompt-v1'],
+    ['I','Run2024I-Prompt-v1'],
+]
+
+
+DataSets = [
+  'MuonEG',
+  'Muon0',
+  'Muon1',
+  'EGamma0',
+  'EGamma1'
+  ]
+
+DataTrig = {
+    'MuonEG'  : 'Trigger_ElMu' ,
+    'Muon0'   : '!Trigger_ElMu && (Trigger_sngMu || Trigger_dblMu)',
+    'Muon1'   : '!Trigger_ElMu && (Trigger_sngMu || Trigger_dblMu)',
+    'EGamma0' : '!Trigger_ElMu && !Trigger_sngMu && !Trigger_dblMu && (Trigger_sngEl || Trigger_dblEl)',
+    'EGamma1' : '!Trigger_ElMu && !Trigger_sngMu && !Trigger_dblMu && (Trigger_sngEl || Trigger_dblEl)',
+}
+
+
+
+samples['DATA'] = {
+  'name': {},
+  'weight': 'LepWPCut',
+  # 'weight': 'LepWPCut*METFilter_DATA',
+  'weights': {},
+  'isData': ['all'],
+  'FilesPerJob': 100
+}
+
+for era, era_name in DataRun:
+  for pd in DataSets:
+    datatag = pd + '_' + era_name
+
+    # get the files
+    if datatag.startswith('MuonEG'):
+        files = nanoGetSampleFiles(dataDirectoryMuonEG, datatag)
+    elif datatag.startswith('Muon'):
+        files = nanoGetSampleFiles(dataDirectoryMuon, datatag)
+    elif datatag.startswith('EGamma'):
+        files = nanoGetSampleFiles(dataDirectoryEGamma, datatag)
+
+    samples['DATA']['name'].update(files)
+
+    # add the weight that is different pd by pd, to take into account orthogonality of triggers
+    samples['DATA']['weights'].update( {datatag : DataTrig[pd] })
+
+
+#
+# Useful later on, like aliases.py, nuisances.py, ...
+#
+
+mcALL     = [skey for skey in samples if skey not in ('DATA', 'Fake_lep')]
+ALL       = [skey for skey in samples]
+
+
+
+
+
